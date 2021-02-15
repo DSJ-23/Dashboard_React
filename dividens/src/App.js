755b8268c29a04c438725053ed564c8ae37b4a52
DanielSilva23 <dannysilva12345@gmail.com>
2021-02-14 22:12:03 -0500

    context not working

--- dividens/src/App.js
+++ dividens/src/App.js
@@ -3,15 +3,21 @@ import World from './components/world';
 import Error from './components/error';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {CountContext} from './components/CountContext';
 
 function App() {
+
+  let count = 0;
+
   return (
     <div className="App App-header">
       <Router>
         <div class="top">
           <Switch>
-            <Route path="/hello" exact component={World}></Route>
-            <Route path="*" component={Error}></Route>
+            <CountContext.Provider value={count}>
+              <Route path="/hello" exact component={World}></Route>
+              <Route path="*" component={Error}></Route>
+            </CountContext.Provider>
           </Switch>
         </div>
       </Router>
@@ -21,5 +27,13 @@ function App() {
 }
 
 export default App;
+
+// <Route path="/hello" exact component={World}></Route>
+// <Route path="*" component={Error}></Route>
+
+// <CountContext.Provider value={count}>
+// <Route path="/hello" exact component={World}></Route>
+// <Route path="*" component={Error}></Route>
+// </CountContext.Provider>
 
 
